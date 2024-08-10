9bd4d1fe8c3308a0196e137a37f91cc2bd56dbe7
noname759 <umarovamirjon832@gmail.com>
2024-08-10 19:21:36 +0300

    main.py

--- main.py
+++ main.py
@@ -1,72 +1,84 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler
+
+TOKEN = '7380800891:AAE9okJyeQ-44fiO3atlO7QmSUjYqpPydQ0'
 
 tasks = {}
 
-def start(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.chat_id
+def start(update, context):
+    user_id = update.message.from_user.id
     if user_id not in tasks:
         tasks[user_id] = []
-    update.message.reply_text('Добро пожаловать!\n'
-                               '\n\n Для получения информации введите /help')
+    message = ('Добро пожаловать!\n'
+                '\n\n Для получения информации введите /help')
+    update.message.reply_text(message)
 
-def add(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.chat_id
-    task = ' '.join(context.args)
-    if task:
-        tasks[user_id].append(task)
+def add(update, context):
+    user_id = update.message.from_user.id
+    task_name = ' '.join(context.args)
+    if task_name:
+        if user_id not in tasks:
+            tasks[user_id] = []
+        tasks[user_id].append(task_name)
         update.message.reply_text(f'Задача "{task}" добавлена.')
+        list(update, context)
     else:
-        update.message.reply_text('Пожалуйста, укажите название задачи.')
-
-def list(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.chat_id
-    if tasks[user_id]:
-        task_list = '\n'.join([f'{i+1}. {task}' for i, task in enumerate(tasks[user_id])])
+        update.message.reply_text('Укажите /add <название задачи>.')
+    
+def list(update, context):
+    user_id = update.message.from_user.id
+    if user_id in tasks and tasks[user_id]:
+        task_list = '\n'.join(f"{i+1}. {task}" for i, task in enumerate(tasks[user_id]))
         update.message.reply_text(f'Ваши задачи:\n{task_list}')
     else:
         update.message.reply_text('У вас нет задач.')
-
-def delete(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.chat_id
+def delete(update, context):
+    user_id = update.message.from_user.id
     try:
-        task_index = int(context.args[0]) - 1
-        removed_task = tasks[user_id].pop(task_index)
-        update.message.reply_text(f'Задача "{removed_task}" удалена.')
+        task_number = int(context.args[0]) - 1 
+        if 0 <= task_number < len(tasks[user_id]):
+            removed_task = tasks[user_id].pop(task_number)  
+            update.message.reply_text(f'Задача "{removed_task}" удалена.')
+            list(update, context) 
+        else:
+            update.message.reply_text('Пожалуйста, укажите правильный номер задачи.')
     except (IndexError, ValueError):
-        update.message.reply_text('Пожалуйста, укажите правильный номер задачи.')
+        update.message.reply_text('Укажите /delete <номер задачи>')
 
-def edit(update: Update, context: CallbackContext) -> None:
-    user_id = update.message.chat_id
+def edit(update, context):
+    user_id = update.message.from_user.id
     try:
-        task_index = int(context.args[0]) - 1
-        new_task = ' '.join(context.args[1:])
-        tasks[user_id][task_index] = new_task
-        update.message.reply_text(f'Задача обновлена на "{new_task}".')
+        task_number = int(context.args[0]) - 1  
+        new_name = ' '.join(context.args[1:])
+        if 0 <= task_number < len(tasks[user_id]) and new_name:
+            old_task = tasks[user_id][task_number]
+            tasks[user_id][task_number] = new_name
+            update.message.reply_text(f'Задача обновлена на "{new_task}".')
+            list(update, context)
+        else:
+            update.message.reply_text('Пожалуйста, укажите правильный номер задачи и новое название.')
     except (IndexError, ValueError):
-        update.message.reply_text('Пожалуйста, укажите правильный номер задачи и новое название.')
+        update.message.reply_text('Укажите /edit <номер> и <название>')
 
-def help(update: Update, context: CallbackContext) -> None:
-    help_text = (
-        "/start - Начать взаимодействие с ботом\n"
+def help(update, context):
+    update.message.reply_text(
+       "/start - Начать взаимодействие с ботом\n"
         "/add <название задачи> - Добавить новую задачу\n"
         "/list - Просмотреть все задачи\n"
         "/delete <номер задачи> - Удалить задачу\n"
         "/edit <номер задачи> <новое название> - Редактировать задачу\n"
         "/help - Показать список команд"
-    )
-    update.message.reply_text(help_text)
+        )
 
-def main() -> None:
-    updater = Updater("7380800891:AAE9okJyeQ-44fiO3atlO7QmSUjYqpPydQ0")
+def main():
+    updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("add", add))
-    dispatcher.add_handler(CommandHandler("list", list))
-    dispatcher.add_handler(CommandHandler("delete", delete))
-    dispatcher.add_handler(CommandHandler("edit", edit))
-    dispatcher.add_handler(CommandHandler("help", help))
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('add', add))
+    dispatcher.add_handler(CommandHandler('list', list))
+    dispatcher.add_handler(CommandHandler('delete', delete))
+    dispatcher.add_handler(CommandHandler('edit', edit))
+    dispatcher.add_handler(CommandHandler('help', help))
 
     updater.start_polling()
     updater.idle()
